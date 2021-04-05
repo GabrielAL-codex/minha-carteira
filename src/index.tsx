1480757a0586c7548361c78f1c12e2e37a94075c
Gavin <gavin@cp.gavin>
2021-04-05 18:36:38 -0300

    Criando index para pages

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import Dashboard from './pages/Dashboard';
+
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
