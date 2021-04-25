c3316d4f36b7b7f641fd934242288df2a05f4b1f
Gavin <gavin@cp.gavin>
2021-04-25 20:49:00 -0300

    Criando Theme Dark e Light

---
+++ src/styles/themes/Light.ts
@@ -0,0 +1,17 @@
+export default {
+    title: 'Light',
+
+    color: {
+        primary: '#DCDCDC',
+        secondary: '#FFF',
+        tetiary: '#F5F5F5',
+
+        white: '#000',
+        black: '#FFF',
+        gray: '#BFBFBF',
+
+        success: '#03BB85',
+        info: '#3B5998',
+        warning: '#FF6961',
+    },
+};
