269468adf338daf4c324ad5896bbe2fae0069359
Gavin <gavin@cp.gavin>
2021-05-23 09:27:23 -0300

    Configurando aleatoriedade dos emojis fe

--- src/Components/MainHeader/index.tsx
+++ src/Components/MainHeader/index.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, {useMemo} from 'react';
 
+import emojis from '../../utils/emojis';
 import { 
     Container, 
     Profile, 
@@ -8,12 +9,18 @@ import {
 } from './styles';
 
 const MainHeader: React.FC = () => {
+    
+    const emoji = useMemo(() => {
+        const indice = Math.floor(Math.random() * emojis.length)
+        return emojis[indice];
+    },[]);
+    
     return (
         <Container>
             <h1>Toogle</h1>
 
             <Profile>
-                <Welcome>Olá,</Welcome>
+                <Welcome>Olá, {emoji}</Welcome>
                 <UserName>Gabriel Almeida</UserName>
             </Profile>
         </Container>
