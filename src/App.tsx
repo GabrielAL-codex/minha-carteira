143b0048edb84b8425c6e9593b00f902d9ca6252
Gavin <gavin@cp.gavin>
2021-05-31 18:13:35 -0300

    Configurando children

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,15 @@ import { ThemeProvider } from 'styled-components';
 import GlobalStyles from './styles/GlobalStyles';
 
 import Layout from './Components/Layout';
+import Dashboard from './pages/Dashboard';
 import Dark from './styles/themes/Dark';
 const App: React.FC = () => {
     return (
         <ThemeProvider theme={Dark}>
             <GlobalStyles />
-            <Layout />
+            <Layout>
+               <Dashboard />
+            </Layout>
         </ThemeProvider>
     );
 
